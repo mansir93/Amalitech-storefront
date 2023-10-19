840c92238d6c90fe4a9c147744e3a7f770267fc6
Mansir <mansiraziz93@gmail.com>
2023-10-19 16:55:45 +0000

    update
    added profile to the dashboard

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -1,9 +1,52 @@
-import React from 'react'
+import { BsArrowDown } from "react-icons/bs";
+import { BsArrowUp } from "react-icons/bs";
+import React from "react";
 
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
-  )
-}
+    <section>
+      <div className="my-4 flex flex-wrap gap-2 flex-col md:flex-row lg:flex-row space-y-4 lg:space-y-0 lg:space-x-4">
+        <div className="flex-1 flex flex-col gap-2 items-center bg-white border-2 border-blue-950 p-4">
+          <h1 className="w-full text-center border-b border-gray-400 text-blue-950 font-bold">
+            {" "}
+            Sites Created
+          </h1>
+          <div className="text-center">2</div>
+        </div>
+        <div className="flex-1 flex flex-col gap-2 items-center bg-white border-2 border-blue-950 p-4">
+          <h1 className="w-full text-center border-b border-gray-400 text-blue-950 font-bold">
+            Views
+          </h1>
+          <div className="text-center">8993</div>
+        </div>
 
-export default Dashboard
+        <div className="flex-1 flex flex-col gap-2 items-center bg-white border-2 border-blue-950 p-4">
+          <h1 className="w-full text-center border-b border-gray-400 text-blue-950 font-bold">
+            Orders
+          </h1>{" "}
+          <div className="text-center">433</div>
+          <div className="w-full flex justify-between items-center">
+            <span className="text-xs text-gray-400">from last period</span>
+            <span className="flex items-center text-red-500  gap-1 text-xs">
+              9.07% <BsArrowDown />
+            </span>
+          </div>
+        </div>
+        <div className="flex-1 flex flex-col gap-2 items-center bg-white border-2 border-blue-950 p-4">
+          <h1 className="w-full text-center border-b border-gray-400 text-blue-950 font-bold">
+            Earnings
+          </h1>{" "}
+          <div className="text-center">GH₵ 4303.00</div>
+          <div className="w-full flex justify-between items-center">
+            <span className=" text-xs text-gray-400">from last period</span>
+            <span className="flex items-center text-green-500  gap-1 text-xs">
+              12.56% <BsArrowUp />{" "}
+            </span>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Dashboard;
--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -1,9 +1,59 @@
-import React from 'react'
+import { FaPen } from "react-icons/fa";
+import React from "react";
+import Avatar from "../Assets/ProfileAvatar.png";
 
 const Profile = () => {
   return (
-    <div>Profile</div>
-  )
-}
+    <section>
+      <div className="w-full flex flex-col justify-center items-center gap-8 p-8">
+        <div className=" flex justify-center items-center gap-8">
+          <img src={Avatar} className="" alt="" />
+          <h1 className="flex items-center gap-2 text-2xl font-bold text-blue-950">
+            Business Name{" "}
+            <span>
+              {" "}
+              <FaPen />
+            </span>
+          </h1>
+        </div>
+        <div className="w-full flex flex-col justify-center items-center gap-8 p-8">
+          <div className="w-full max-w-md flex flex-col bg-white shadow-md p-2">
+            <label className=" text-gray-500">Email</label>
+            <span className="flex justify-between items-center gap-2 ">
+              <input
+                className="text-blue-950 border-none outline-none"
+                value="business.and@gmail.com"
+              />{" "}
+              <FaPen />
+            </span>
+          </div>{" "}
+          <div className="w-full max-w-md flex flex-col bg-white shadow-md p-2">
+            <label className=" text-gray-500">Contact</label>
+            <span className="flex justify-between items-center gap-2 ">
+              <input
+                className="text-blue-950 border-none outline-none"
+                value="+233 247 000 000"
+              />{" "}
+              <FaPen />
+            </span>
+          </div>
+          <div className="w-full max-w-md flex flex-col bg-white shadow-md p-2">
+            <label className=" text-gray-500">Location</label>
+            <span className="flex justify-between items-center gap-2 ">
+              <input
+                className="text-blue-950 border-none outline-none"
+                value="10th Avenue Ron Island Street Takoradi, Ghana"
+              />{" "}
+              <FaPen />
+            </span>
+          </div>
+          <div className="w-full max-w-md flex justify-end items-end p-2  ">
+            <button className="text-white bg-blue-950 py-4 px-8">Save</button>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default Profile
+export default Profile;
