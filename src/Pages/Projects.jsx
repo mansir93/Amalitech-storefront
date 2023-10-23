d12e6574a15ba54918026e6943d75d78fcd2ebc7
Kelvin Akyea <akyeaniv@gmail.com>
2023-10-23 14:28:11 +0000

    Added projects section

--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -1,8 +1,37 @@
 import React from 'react'
+import ImageFile from '../Assets/ImageFile.png'
+import AddIcon from '../Assets/AddIcon.png'
+import ProjectImage1 from '../Assets/ProjectImage1.png'
+import ProjectImage2 from '../Assets/ProjectImage2.png'
 
 const Projects = () => {
   return (
-    <div>Projects</div>
+    <div>
+      <div className="flex-1 flex flex-row w-48 items-center bg-white border-2 border-blue-950 p-4">
+        <div>
+          <img src={ImageFile} alt='' />
+        </div>
+          <h1 className="w-full text-center text-blue-950 font-bold">
+            New Project
+          </h1>
+          <div>
+          <img src={AddIcon} alt='' />
+          </div>
+      </div>
+      <div className='text-xl border-t border-gray-400 mt-6 font-bold'>
+        <p className='pt-3'>Recent</p>
+      </div>
+      <div className='w-full flex flex-row gap-10 pt-3'>
+          <div>
+          <img src={ProjectImage1} alt='' />
+          <p className='text-center'>Laso Blog</p>
+          </div>
+          <div>
+          <img src={ProjectImage2} alt='' />
+          <p className='text-center'>Mountain Blog</p>
+          </div>
+      </div>
+    </div>
   )
 }
 
