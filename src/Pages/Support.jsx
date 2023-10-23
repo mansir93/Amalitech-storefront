45cd707c8aaf892bd4dc0cee506fcc94242b7ee4
obeng44 <147715260+obeng44@users.noreply.github.com>
2023-10-23 14:42:15 +0000

    Update Support.jsx

--- src/Pages/Support.jsx
+++ src/Pages/Support.jsx
@@ -4,7 +4,7 @@ const Support = () => {
   return <div>
     <div>
     <form>
-      <textarea className="text-gray-300 w-[90%] p-5 text-sm">Hello Storefront, I would to know about you:</textarea>
+      <textarea className="text-gray-300 min-h-[350px] shadow-md w-[90%] p-5 text-sm">Hello Storefront, I would to know about you:</textarea>
       <button className="flex flex-col bg-slate-800 text-white py-1 px-10 w-[10%] ">Send</button>
     </form>
     </div>
