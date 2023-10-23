00fbb89ed8e297da73a01df5e7c82174441c06a9
obeng44 <147715260+obeng44@users.noreply.github.com>
2023-10-23 14:38:22 +0000

    Update Support.jsx

--- src/Pages/Support.jsx
+++ src/Pages/Support.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const Support = () => {
-  return <div>Support</div>;
+  return <div>
+    <div>
+    <form>
+      <textarea className="text-gray-300 w-[90%] p-5 text-sm">Hello Storefront, I would to know about you:</textarea>
+      <button className="flex flex-col bg-slate-800 text-white py-1 px-10 w-[10%] ">Send</button>
+    </form>
+    </div>
+  </div>;
 };
 
 export default Support;
