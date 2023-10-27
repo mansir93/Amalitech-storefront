5c19b247ad86665ba5d8134a51495dee93fdb3a2
obeng44 <147715260+obeng44@users.noreply.github.com>
2023-10-27 11:49:05 +0000

    Update Hero.jsx

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -5,14 +5,13 @@ import { Link } from "react-router-dom";
 const Hero = () => {
   return (
     <section className="bg-gray-100">
-      <div className="min-h-[60vh] min-w-[30vw] flex flex-col lg:flex-row container mx-auto py-8 px-2">
+      <div className="min-h-[60vh] flex flex-col lg:flex-row container mx-auto py-8 px-2">
         <div className="flex-1 flex flex-col gap-8 py-20">
-          <h1 className="text-4xl font-black">Frontend Team</h1>
-          <h1 className="text-4xl font-black">And Some Outfits </h1>
-          <p>
-            Business Deal Of
-            <br />
-           Delivery Products And Services
+          <h1 className="text-5xl font-black">Frontend Team</h1>
+          <h1 className="text-5xl font-black">And Some Outfits </h1>
+          <p className="text-xl">
+            Business Deal Of Delivery <br />
+            Products And Services
           </p>
           <Link
             to="/signup"
